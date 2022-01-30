d91209a1dfdf51f7b80fb546ce389ae9f0e1df28
aniketltoothsi <aniket.lavand@toothsi.in>
2022-01-30 20:12:50 +0530

    day6 changes

--- day6JS/index.js
+++ day6JS/index.js
@@ -1,13 +1,23 @@
-window.history.pushState(null,"",window.location.href);
 
-window.onpopstate = function () {
-    window.history.pushState(null, "", window.location.href);
-};
+var dialog=document.getElementById('dia');
 
+document.getElementById('btn').addEventListener('click',function onOpen(){
+  history.pushState(null, null, window.location.href);
+      window.onpopstate = function () {
+     history.go(1);     }
+  if(typeof dialog.showModal === "function" ){
+    dialog.showModal();
+  }
+})
 
-
-
-document.getElementById('danger').addEventListener("click",()=>{
-    alert("warning!!!");
+inner.addEventListener('click',function onClose(){
+  // var length = history.length;
+  //    history.go(-length);
+  dialog.close();
+      window.onpopstate=function(){
+        window.location.replace("file:///home/ampa/training/day6JS/index.html")
+      }
+      
+      
+  
 });
-
