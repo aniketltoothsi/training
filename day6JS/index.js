760b072b54cb639f96ee93c34838e9068e70f9ad
aniketltoothsi <aniket.lavand@toothsi.in>
2022-01-31 14:42:46 +0530

    adding dialog

--- day6JS/index.js
+++ day6JS/index.js
@@ -3,21 +3,21 @@ var dialog=document.getElementById('dia');
 
 document.getElementById('btn').addEventListener('click',function onOpen(){
   history.pushState(null, null, window.location.href);
-      window.onpopstate = function () {
-     history.go(1);     }
+      
   if(typeof dialog.showModal === "function" ){
     dialog.showModal();
   }
 })
-
+window.onpopstate=function(){
+ dialog.close()       
+}
 inner.addEventListener('click',function onClose(){
-  // var length = history.length;
-  //    history.go(-length);
+
   dialog.close();
-      window.onpopstate=function(){
-        window.location.replace("file:///home/ampa/training/day6JS/index.html")
-      }
+  history.back(); 
       
       
   
 });
+
+
